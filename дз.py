7f60b9531fd09208f4606148309ef99423387735
Anastasia <bogdanovanastja@yandex.ru>
2022-12-28 02:22:35 +0300

    дз4

--- дз.py
+++ дз.py
@@ -1,86 +1,137 @@
-# ЗАДАЧИ К СЕМИНАРУ 3
-# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на позиции с нечетным индексом.
+# ЗАДАЧИ К СЕМИНАРУ 4 
+
+# A. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) 
+# многочлена и записать в файл многочлен степени k.
 # Пример:
-# [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
-# import random
-# list = []
-# count = 0
-# for i in range(10):
-#     list.append(random.randint(0,9))
-#     if i%2 !=0:
-#         count+=list[i]
-# print(f'Наш получившийся список: {list}')
-# print(f'Cумма элементов с нечетных позиций: {count}')
+# если k = 2, то многочлены могут быть => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-# Пример:
-# [2, 3, 4, 5, 6] => [12, 15, 16];
-# [2, 3, 5, 6] => [12, 15]
-
-# import random
-# list = []
-# count = []
-# for i in range(random.randint(3,15)):
-#     list.append(random.randint(0,9))
-# len1 = len(list)/2
-# if len1%2 != 0:
-#     len1+=0.5
-# len1=int(len1)   
-# for i in range(1,len1+1):
-#     count.append(list[i-1]*list[-i])
-# print(len1)
-# print(f'Первый список: {list}')
-# print(f'Получившееся произведение: {count}')
+import random
+k1 = int(input('Введите натуральную степень для уравнения 1: '))
+k2 = int(input('Введите натуральную степень для уравнения 2: '))
+# k1 = 3 
+# k2 = 2
 
 
-# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и 
-# минимальным значением дробной части элементов, отличной от 0.
-# Пример:
-# [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+def eq (k: int):
+    eqq = []
+    kk = k
+    for i in range(k):
+        temp = random.randint(-100, 100)
+        if temp == 0:
+            if k==kk:
+                eqq.append(f'x**{k}')
+                k-=1
+            elif k!=kk and k!=0:
+                k-=1
+        elif temp == 1:
+            eqq.append(f'x**{k}')
+            k-=1
+        elif temp == -1:
+            eqq.append(f'-x**{k}')
+            k-=1
+        else:
+            if k > 1:
+                eqq.append(f'{temp}*x**{k}')
+                k-=1 
+            if k == 0:
+                eqq.append(f'{temp}')
+                break
+            if k == 1:
+                eqq.append(f'{random.randint(-100, 100)}*x')
+                k-=1            
+      
+    return eqq
+
+def conv (eq1: list):
+    eq11 = ''
+    for el in eq1:
+        if el == eq1[len(eq1)-1]:
+            eq11 += str(f'{el} = 0') 
+            break
+        else:
+            eq11 += str(f'{el} + ')
+        
+    eq11 = eq11.replace('+ -', ' - ')  
+    return eq11
+
+eq1 = eq(k1)
+eq2 = eq(k2)
+eq11 = conv(eq1)
+eq22 = conv(eq2)
+
+print(f'Первое уравнение: {eq11}')
+print(f'Второе уравнение: {eq22}')
+
+# B. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий 
+# сумму многочленов.
+
+# print(f'ПервЫЙ многочлен в виде списка: {eq1}')
+# print(f'Второй многочлен в виде списка: {eq2}')
+
+def Create_dict (eq: list):
+    dict = {}
+    for el in eq:
+        if '**' in el:
+            if '*x**' in el:
+                el = el.split('*x**')
+                dict[int(el[1])] = int(el[0])
+            else:
+                el = el.split('x**')
+                if el[0] == '-':
+                    dict[int(el[1])] = int(-1)  
+                else:
+                    dict[int(el[1])] = int(1) 
+        elif 'x' in el and "**" not in el:
+            el = el.split('*x')
+            dict[1] = int(el[0])
+        else:
+            dict[0] = int(el)
+    return dict
+
+def Combine (dict1: dict, dict2: dict):
+    keys = set()
+    for key in dict1:
+        keys.add(key)
+    for key in dict2:
+        keys.add(key)
+    for key in keys:
+        dict1[key] = dict1.get(key,0) + dict2.get(key,0) 
+    return dict1
 
 
-# import random
-# list = []
-# for i in range(random.randint(3,9)):
-#     list.append(round(random.random()*100,2))
-# list2 = []
-# for i in range(len(list)):
-#     if list[i] != int(list[i]):
-#         list2.append(round(list[i]-int(list[i]), 2))
-   
-# count = max(list2)-min(list2)
+dict1 = Create_dict (eq1)
+dict2 = Create_dict (eq2)
+# print(f'словарь1: {dict1}')
+# print(f'словарь2: {dict2}')
+dict1 = Combine (dict1, dict2)
+# print(f'словарь новый: {dict1}')
+        
+a = []
+b = []
+for key in dict1:
+        a.append(key)
+a.sort(reverse=True)
 
-# print(f'наш получившийся список: {list}')
-# print(f'разница max и min дробных частей: {round(count, 2)}')
-
-
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-# Пример:
-# 45 -> 101101
-# 3 -> 11
-# 2 -> 10
-
-# num = int(input('Введите число: '))
-# num2 = []
-# while num>0:
-#     if num%2 == 0:
-#         num2.append(0)
-#     else:
-#          num2.append(1)
-#     num= num//2
-# num2.reverse() 
-# print(f'В двочиной системе число {num} будет выглядеть так: ', end = " ")
-# print(*num2)
-
-# Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
-# Пример:
-# для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] Негафибоначчи
-# num = int(input('Введите число: '))
-# fib_list = [1, 0, 1]
-# if num>1:
-#     for i in range (1, num):
-#         fib_list.append(fib_list[i+1] +fib_list[i])
-#     for i in range (1, num):
-#         fib_list.insert(0, (fib_list[1] -fib_list[0]))
-# print(fib_list)
-
+for el in a:
+    if el == 0:
+        b.append(dict1[el])
+        break
+    if el == 1:
+        if dict1[el] !=0:
+            if dict1[el] == 1:
+                b.append('x')
+            if dict1[el] == -1:
+                b.append('-x')
+            else:
+                b.append(f'{dict1[el]}*x')
+    else:
+        if dict1[el] !=0:
+            if dict1[el] == 1:
+                b.append(f'x**{el}')
+            elif dict1[el] == -1:
+                b.append(f'-x**{el}')
+            else:
+                b.append(f'{dict1[el]}*x**{el}')
+b = conv(b)
+print(f'Cумма многочленов равна: {b}')
+    
